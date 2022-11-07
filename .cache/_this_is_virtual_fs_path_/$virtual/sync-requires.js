5709bedd3d6d66124beda2a0f9a252af8c948052
Tania Griffith <tania.griffith@braze.com>
2022-11-07 16:27:51 -0500

    build it

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/taniagriffith/Sites/localhost/gatsby/my-first-gatsby-site/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/taniagriffith/Sites/localhost/gatsby/my-first-gatsby-site/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/taniagriffith/Sites/localhost/gatsby/my-first-gatsby-site/src/pages/index.js"))
 }
